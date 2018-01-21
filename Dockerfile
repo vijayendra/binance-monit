2df1d9ac726537a263effaa4362f546927765bdd
Vijayendra Bapte <vbapte@egnyte.com>
2018-01-21 01:00:59 -0800

    Creating web server

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,13 @@
 FROM python:3
 
+RUN apt-get update && \
+    apt-get install -y ntp && \
+    ntpd -gq && \
+    service ntp start
+
 WORKDIR /opt/app
 
 COPY requirements.txt .
-COPY config.js .
 RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 CMD [ "python", "./query.py" ]
--- query.py
+++ query.py
@@ -1,54 +1,122 @@
-#!/usr/bin/env python
-import getpass
-import json
+import asyncio
+import itertools
 import os
+import time
+from pprint import pprint
 
+import binance
 from binance.client import Client
 
-CONFIG_PATH = '/opt/app/config.js'
+import yaml
+from aiohttp import web
+
+CONFIG_PATH = '/opt/app/config.yaml'
+
+if not os.path.isfile(CONFIG_PATH):
+    raise SystemError('Config file not exists: {}'.format(CONFIG_PATH))
 
 
-def get_creds():
-    if os.path.isfile(CONFIG_PATH):
-        with open(CONFIG_PATH) as fptr:
-            data = json.load(fptr)
-        key = data['exchanges']['binance']['key']
-        secret = data['exchanges']['binance']['secret']
-    else:
-        key = getpass.getpass('Enter api key: ')
-        secret = getpass.getpass('Enter api secret: ')
-    return key, secret
+def get_config():
+    with open(CONFIG_PATH, 'r') as stream:
+        try:
+            return yaml.load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+            raise SystemError(
+                'Error while reading config: {}'.format(CONFIG_PATH))
 
 
-def main():
-    api_key, api_secret = get_creds()
-    client = Client(api_key, api_secret)
-    info = client.get_account()
-    balances = info['balances']
-    btc_deposits = client.get_deposit_history(asset='BTC')
-    btc_deposit_amount = 0.0
-    for deposit in btc_deposits['depositList']:
-        if deposit['status'] == 1 and deposit['asset'] == 'BTC':
-            btc_deposit_amount += deposit['amount']
-    tickers = client.get_all_tickers()
-    prices = {}
-    for ticker in tickers:
-        prices[ticker['symbol']] = float(ticker['price'])
-    # set a base trade rate
-    prices['BTCBTC'] = 1.0
-    btc_balance = 0.0
-    for asset in balances:
-        symbol = asset['asset'] + 'BTC'
-        position = (float(asset['free']) + float(asset['locked']))
-        if position > 0.00001:
-            btc_balance += position * prices[symbol]
-    print("Account value: {} BTC".format(btc_balance))
-    print("Raw input: {} BTC".format(btc_deposit_amount))
-    print("Net Profit: {} BTC".format((btc_balance - btc_deposit_amount)))
-    print("USD value: {}".format((btc_balance * prices['BTCUSDT'])))
-    print("USD profit: {}".format(
-          ((btc_balance - btc_deposit_amount) * prices['BTCUSDT'])))
+class Binance(object):
+    def __init__(self, app):
+        self.config = app['config']
+        self.client = Client(
+            self.config['binance']['key'],
+            self.config['binance']['secret'],
+        )
+
+    async def query(self):
+        data = {
+            'time': time.time(),
+            'total_invtestment': self.config['total_invtestment']
+        }
+        info = self.client.get_account()
+        balances = info['balances']
+        btc_deposits = self.client.get_deposit_history(asset='BTC')
+        btc_deposit_count = 0
+        for deposit in btc_deposits['depositList']:
+            if deposit['status'] == 1 and deposit['asset'] == 'BTC':
+                btc_deposit_count += deposit['amount']
+        tickers = self.client.get_all_tickers()
+        prices = {}
+        for ticker in tickers:
+            prices[ticker['symbol']] = float(ticker['price'])
+        # set a base trade rate
+        prices['BTCBTC'] = 1.0
+        btc_balance = 0.0
+        for asset in balances:
+            symbol = asset['asset'] + 'BTC'
+            position = (float(asset['free']) + float(asset['locked']))
+            if position > 0.00001:
+                btc_balance += position * prices[symbol]
+        btc_delta = btc_balance - btc_deposit_count
+        data['btc_usd_amount'] = prices['BTCUSDT']
+        data['btc_deposit'] = {
+            'count': btc_deposit_count,
+            'amount': self.config['btc_deposit_amount'],
+        }
+        data['btc_now'] = {
+            'count': btc_balance,
+            'amount': btc_balance * prices['BTCUSDT'],
+        }
+        data['btc_delta'] = {
+            'count': btc_delta,
+            'amount': btc_delta * prices['BTCUSDT'],
+        }
+        data['net_profit'] = (btc_balance * prices['BTCUSDT']) - \
+            self.config['btc_deposit_amount']
+        return data
+
+
+async def query(app):
+    binance_obj = Binance(app)
+    try:
+        for i in itertools.count():
+            print(i)
+            try:
+                data = await binance_obj.query()
+            except binance.exceptions.BinanceAPIException as e:
+                print('Error {}'.format(e))
+            else:
+                pprint(data)
+            await asyncio.sleep(5)
+    except asyncio.CancelledError:
+        pass
+    finally:
+        print('Done Query')
+
+
+async def start_background_tasks(app):
+    app['query_binance'] = app.loop.create_task(query(app))
+
+
+async def cleanup_background_tasks(app):
+    app['query_binance'].cancel()
+    await app['query_binance']
+
+
+async def handle(request):
+    name = request.match_info.get('name', "Anonymous")
+    text = "Hello, " + name
+    return web.Response(text=text)
 
 
 if __name__ == "__main__":
-    main()
+    config = get_config()
+    app = web.Application()
+    app['config'] = config
+    app.router.add_get('/', handle)
+    app.router.add_get('/{name}', handle)
+
+    app.on_startup.append(start_background_tasks)
+    app.on_cleanup.append(cleanup_background_tasks)
+    web.run_app(app)
